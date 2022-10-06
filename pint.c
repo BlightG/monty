d3694fceb4ca3ca19138358a21f8a2e567590630
Birhane Gulilat <birhane.gulilat@gmail.com>
2022-10-06 21:58:58 +0300

    conntiniuation

--- pint.c
+++ pint.c
@@ -4,8 +4,8 @@ void pint(stack_t **TOP, unsigned int line_count)
 {
 	if (*TOP == NULL)
 	{
-		fprintf(stderr, "L<%d>:\
-			can't pint, stack empty\n", line_count);
+		fprintf(\
+		stderr, "L<%d>: can't pint, stack empty\n", line_count);
 		freestack(*TOP);
 		exit(EXIT_FAILURE);
 	}
